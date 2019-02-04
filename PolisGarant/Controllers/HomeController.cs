d977440acc4db3dd3d619841a404213689642c01
Mykola Kotsabiuk <kotsabiukmv98@ukr.net>
2019-02-04 23:54:12 +0200

    Delete old favicon and logo. Added services page and controler

--- PolisGarant/Controllers/HomeController.cs
+++ PolisGarant/Controllers/HomeController.cs
@@ -25,6 +25,11 @@ namespace PolisGarant.Controllers
             return View();
         }
 
+        public IActionResult Services()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
