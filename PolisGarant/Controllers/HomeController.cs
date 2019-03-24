8e8d588d25081796d292d8ca846d5930a06bce08
Mykola Kotsabiuk <mykola.kotsabiuk@3shape.com>
2019-03-24 21:02:04 +0200

    Create a letters sending

--- PolisGarant/Controllers/HomeController.cs
+++ PolisGarant/Controllers/HomeController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
 using PolisGarant.Models;
 
 namespace PolisGarant.Controllers
@@ -26,6 +28,29 @@ namespace PolisGarant.Controllers
         {
             ViewBag.Tab = "Contacts";
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendMessage(string name, string email, string message)
+        {
+            var messageForSending = new MimeMessage();
+            messageForSending.From.Add(new MailboxAddress(name, email));
+            messageForSending.To.Add(new MailboxAddress("Site PolisGarant", "polisgarant@email.cz"));
+            messageForSending.Subject = "PolisGarant purchase";
+            messageForSending.Body = new TextPart("plain")
+            {
+                Text = message
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587, false);
+                client.Authenticate("polisgarant.cz@gmail.com", "polisgarant.cz2019");
+                client.Send(messageForSending);
+                client.Disconnect(true);
+            }
+
+            return View(@"~/Views/Home/Index.cshtml");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
